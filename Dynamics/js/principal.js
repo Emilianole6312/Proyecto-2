751afe793cd7f95b7bc1c6478674004386c0013a
FernandaVillafan <49957692+FernandaVillafan@users.noreply.github.com>
2020-06-16 23:44:17 -0500

    FEAT: Archivos del proyecto hasta ahorita

--- Dynamics/js/principal.js
+++ Dynamics/js/principal.js
@@ -13,60 +13,48 @@ let dos = document.getElementsByClassName("dos");
 let tres = document.getElementsByClassName("tres");
 let cuatro = document.getElementsByClassName("cuatro");
 
-/*Se muestra juego 1*/
-fetch("../Templates/Blackjack.html")
-  .then((response) => {
+gameone.onclick = function() {
+  fetch("../Templates/blackjack.html")
+  .then((response => {
     return response.text();
+  }))
+  .then((data) => {
+    contenedorJuego.innerHTML = data;
+    $("#jugar").click(blackjackgame);
   })
-  .then((text) => {
-    console.log(text);
-    document.getElementById("gameone").addEventListener("click", () => {
-      document.getElementById("contenedorJuego").innerHTML = text;
-    })
+  .catch((error) => {
+    console.error(error);
   })
+};
 
+// gametwo.onclick = function() {
+//     $(contenedorJuego).empty();
+//     contenedorJuego.appendChild(otro);
+// }
+//
+// gamethree.onclick = function() {
+//     $(contenedorJuego).empty();
+//     contenedorJuego.appendChild(otroDos);
+// }
 
-/*let text = document.createElement("p");
-text.innerText = "Algo";*/
-
-let otro = document.createElement("p");
-otro.innerText = "Juego 2";
-
-let otroDos = document.createElement("p");
-otroDos.innerText = "Juego 3";
-
-/*gameone.onclick = function() {
-    $(contenedorJuego).load(text);
-    contenedorJuego.appendChild(text);
-};*/
-
-gametwo.onclick = function() {
-    $(contenedorJuego).empty();
-    contenedorJuego.appendChild(otro);
-}
-
-gamethree.onclick = function() {
-    $(contenedorJuego).empty();
-    contenedorJuego.appendChild(otroDos);
-}
 claro.onclick = function() {
-    $(navbar).css("background-color", "#389448");
-    $(lateral).css("background-color", "#D4CC62");
-    $(content).css("background-color", "#D4CC62");
-    $(uno).css("background-color", "#D94223");
-    $(dos).css("background-color", "#F69221");
-    $(tres).css("background-color", "#91B41E");
-    $(cuatro).css("background-color", "#F3401F");
-    document.body.style.backgroundColor = "#D4CC62";
+  $(navbar).css("background-color", "#389448");
+  $(lateral).css("background-color", "#D4CC62");
+  $(content).css("background-color", "#D4CC62");
+  $(uno).css("background-color", "#D94223");
+  $(dos).css("background-color", "#F69221");
+  $(tres).css("background-color", "#91B41E");
+  $(cuatro).css("background-color", "#F3401F");
+  document.body.style.backgroundColor = "#D4CC62";
 }
 
 oscuro.onclick = function() {
-    $(navbar).css("background-color", "#4169E1");
-    $(lateral).css("background-color", "#4A4B71");
-    $(content).css("background-color", "#4A4B71");
-    $(uno).css("background-color", "#C4619F");
-    $(dos).css("background-color", "#4169E1");
-    $(tres).css("background-color", "#02BF71");
-    $(cuatro).css("background-color", "#BF02AF");
-    document.body.style.backgroundColor = "#4A4B71";
+  $(navbar).css("background-color", "#4169E1");
+  $(lateral).css("background-color", "#4A4B71");
+  $(content).css("background-color", "#4A4B71");
+  $(uno).css("background-color", "#C4619F");
+  $(dos).css("background-color", "#4169E1");
+  $(tres).css("background-color", "#02BF71");
+  $(cuatro).css("background-color", "#BF02AF");
+  document.body.style.backgroundColor = "#4A4B71";
 }
